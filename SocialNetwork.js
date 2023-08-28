4349359104d673f14ae100a6b83690cb6c12368f
Rupie17 <johnruperto17@gmail.com>
2023-08-28 14:11:31 -0700

    first commit

---
+++ SocialNetwork.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { get } from './mockBackend/fetch';
+
+export default function SocialNetwork() {
+    // Split each state and effect into it's own section of code to make it easier to manage and test.
+    // Initially, these were all combined.
+  const [menu, setMenu] = useState(null);
+  useEffect(() => {
+    get('/menu').then(response => setMenu(response.data));
+  }, []);
+
+  const [newsFeed, setNewsFeed] = useState(null);
+  useEffect(() => {
+    get('/news-feed').then(response => setNewsFeed(response.data));
+  }, []);
+  
+  const [friends, setFriends] = useState(null);
+  useEffect(() => {
+    get('/friends').then(response => setFriends(response.data));
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>My Network</h1>
+      {!menu ? <p>Loading..</p> : (
+        <nav>
+          {menu.map((menuItem) => (
+            <button key={menuItem}>{menuItem}</button>
+          ))}
+        </nav>
+      )}
+      <div className='content'>
+        {!newsFeed ? <p>Loading..</p> : (
+          <section>
+            {newsFeed.map(({ id, title, message, imgSrc }) => (
+              <article key={id}>
+                <h3>{title}</h3>
+                <p>{message}</p>
+                <img src={imgSrc} alt='' />
+              </article>
+            ))}
+          </section>
+        )}
+        {!friends ? <p>Loading..</p> : (
+          <aside>
+            <ul>
+              {friends
+                .sort((a, b) => (a.isOnline && !b.isOnline ? -1 : 0))
+                .map(({ id, name, isOnline }) => (
+                  <li key={id} className={isOnline ? 'online' : 'offline'}>
+                    {name}
+                  </li>
+                ))}
+            </ul>
+          </aside>
+        )}
+      </div>
+    </div>
+  );
+}
